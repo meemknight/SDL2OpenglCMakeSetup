894605dd9bcb1bd23ad2b3e0b4d86b6c6b325f32
vlod <vladalex420@gmail.com>
2024-07-10 14:10:06 +0300

    added imgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <SDL2/SDL.h>
 #include <glad/glad.h>
 #include <iostream>
-#include <gl2d/gl2d.h> //my 2d library, just to try opengl
+#include <gl2d/gl2d.h> //my 2d library, just to try OpenGL
+#include <openglErrorReporting.h>
 #undef main
 
+#include "imgui.h"
+#include "backends/imgui_impl_sdl2.h"
+#include "backends/imgui_impl_opengl3.h"
+#include "imguiThemes.h"
 
 int main(int argc, char *argv[])
 {
@@ -18,18 +23,15 @@ int main(int argc, char *argv[])
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 
 #ifdef __APPLE__
-	//apple moment...
+	// apple moment...
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, SDL_GL_CONTEXT_FORWARD_COMPATIBLE_FLAG);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
 	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
 #endif
 
-
-
 	// Create a window
-	SDL_Window *window = SDL_CreateWindow("Hello SDL", 100, 100, 680, 480, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE 
-	| SDL_WINDOW_OPENGL);
+	SDL_Window *window = SDL_CreateWindow("Hello SDL", 100, 100, 680, 480, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE | SDL_WINDOW_OPENGL);
 	if (window == nullptr)
 	{
 		std::cerr << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
@@ -57,6 +59,30 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
+	// Enable OpenGL error reporting
+	enableReportGlErrors();
+
+#pragma region imgui
+	ImGui::CreateContext();
+	// Set ImGui theme
+	imguiThemes::yellow();
+
+	ImGuiIO &io = ImGui::GetIO(); (void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;       // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
+
+	ImGuiStyle &style = ImGui::GetStyle();
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		//style.WindowRounding = 0.0f;
+		style.Colors[ImGuiCol_WindowBg].w = 0.f;
+		style.Colors[ImGuiCol_DockingEmptyBg].w = 0.f;
+	}
+
+	ImGui_ImplSDL2_InitForOpenGL(window, glContext);
+	ImGui_ImplOpenGL3_Init("#version 330");
+#pragma endregion
 
 	// Create a renderer
 	SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
@@ -68,7 +94,7 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	//my 2D library, just to check that opengl works! you can remove it later
+	// Initialize 2D renderer
 	gl2d::init();
 	gl2d::Renderer2D renderer2d;
 	renderer2d.create();
@@ -83,51 +109,72 @@ int main(int argc, char *argv[])
 		renderer2d.updateWindowMetrics(w, h);
 		glViewport(0, 0, w, h);
 
-
 		SDL_Event event;
 		while (SDL_PollEvent(&event))
 		{
+			ImGui_ImplSDL2_ProcessEvent(&event);
 			if (event.type == SDL_QUIT)
 			{
 				running = false;
 			}
+
+			if (event.type == SDL_WINDOWEVENT)
+			{
+				if (event.window.event == SDL_WINDOWEVENT_CLOSE && 
+					event.window.windowID == SDL_GetWindowID(window))
+				{
+					running = false;
+				}
+			}
 		}
 
-		// either draw with openl or their renderer
-		if(0)
+	#pragma region imgui
+		ImGui_ImplOpenGL3_NewFrame();
+		ImGui_ImplSDL2_NewFrame(window);
+		ImGui::NewFrame();
+
+		// Create a docking space
+		ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+
+		// Example ImGui window
+		ImGui::Begin("Test Window");
+		ImGui::Text("Hello, world!");
+		ImGui::Button("I am Pressy the button!");
+		float values[5] = {0.5, 0.4, 0.3, 0.56, 0.46};
+		ImGui::PlotHistogram("I am a plot!", values, 5);
+		ImGui::End();
+	#pragma endregion
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Render a rectangle using OpenGL or 2D renderer
+		renderer2d.renderRectangle({100, 100, 100, 100}, Colors_Orange);
+		renderer2d.flush();
+
+	#pragma region imgui
+		ImGui::Render();
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		// Update and render additional platform windows
+		if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
 		{
-			SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-			SDL_RenderClear(renderer);
-
-			SDL_Rect square = {};
-			square.x = 350;  // x position
-			square.y = 250;  // y position
-			square.w = 100;  // width
-			square.h = 100;  // height
-
-			// Render the square
-			SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-			SDL_RenderFillRect(renderer, &square);
-			
-			// Present the backbuffer
-			SDL_RenderPresent(renderer);
+			SDL_Window *backup_current_window = SDL_GL_GetCurrentWindow();
+			SDL_GLContext backup_current_context = SDL_GL_GetCurrentContext();
+			ImGui::UpdatePlatformWindows();
+			ImGui::RenderPlatformWindowsDefault();
+			SDL_GL_MakeCurrent(backup_current_window, backup_current_context);
 		}
-		else
-		{
-			glClear(GL_COLOR_BUFFER_BIT);
+	#pragma endregion
 
-			renderer2d.renderRectangle({100,100,100,100}, Colors_Orange);
-			renderer2d.flush();
-			SDL_GL_SwapWindow(window);
-
-		}
-
-	
-
-
+		SDL_GL_SwapWindow(window);
 	}
 
-	// Cleanup
+	// Cleanup ImGui
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplSDL2_Shutdown();
+	ImGui::DestroyContext();
+
+	// Cleanup SDL
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
 	SDL_Quit();
